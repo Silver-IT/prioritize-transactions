f91544b8becd501e614dcbe2652c2c80426b33e5
silver-it <alex.jin220@gmail.com>
2022-07-16 05:39:30 +0800

    feat: added requried functions for workflow integration

--- index.ts
+++ index.ts
@@ -10,23 +10,49 @@ interface Transaction {
   bank_country_code: string;
 }
 
+interface FraudCheckedTransaction {
+  id: string;
+  fraudulent: boolean;
+}
+
 async function prioritize(
   transactions: Array<Transaction>,
   totalTime: number = 1000
 ): Promise<Array<Transaction>> {
   totalTime = totalTime < 0 || totalTime > 1000 ? 1000 : totalTime;
-  console.log('Total Time:', totalTime);
+  console.log("Total Time:", totalTime);
 
   return [];
+}
+
+function processTransaction(transaction: Transaction): boolean {
+  return true;
+}
+
+function processTransactions(
+  transactions: Array<Transaction>
+): Array<FraudCheckedTransaction> {
+  const results: Array<FraudCheckedTransaction> = [];
+  for (const transaction of transactions) {
+    results.push({
+      id: transaction.id,
+      fraudulent: processTransaction(transaction),
+    });
+  }
+
+  console.log("Result:", results);
+
+  return results;
 }
 
 async function main() {
   const transactions: Array<Transaction> = await csv().fromFile(
     TRANSACTION_PATH
   );
-  
+
   const priorTransactions: Array<Transaction> = await prioritize(transactions);
-  console.log(priorTransactions);
+
+  processTransactions(priorTransactions);
 }
 
 main();
